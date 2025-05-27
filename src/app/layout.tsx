9b52c12fede1ce6cb63e32079965efe86c77e7f9
Zakk-Fast <dev@zakkfast.io>
2025-05-27 01:20:06 -0500

    adjust layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Header />
+        <div className="header-spacer" />``
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,30 @@
+import Image from "next/image";
+import SDVButton from "./components/button/SDVbutton";
+
 import styles from "./page.module.scss";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles["page__main"]}>
-        <h1 className={styles["page__title"]}>Plandew Valley</h1>
+        <Image
+          src="/image/plandewlogo.png"
+          alt="Plandew Valley Logo"
+          height={200}
+          width={200}
+          style={{ width: "auto", height: "200px", display: "block" }}
+          unoptimized
+        />
+
         <p className={styles["page__subtitle"]}>
           An interactive crop planner and profitability tool for Stardew Valley.
         </p>
-        <a className={styles["page__cta"]} href="/planner">
-          Start Planning
-        </a>
+        <SDVButton>
+          <Link className={styles["page__cta"]} href="/planner">
+            Start Planning
+          </Link>
+        </SDVButton>
       </main>
     </div>
   );
